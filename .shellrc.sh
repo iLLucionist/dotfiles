fd95fd23647ca69a798367ff41c87b1a1b45d289
Maxim Laurijssen <maxim.laurijssen@gmail.com>
2019-06-28 15:30:08 +0200

    Initial commit of dotfiles

---
+++ .shellrc.sh
@@ -0,0 +1,12 @@
+alias dotf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias ls='ls --color=auto'
+alias xq='xbps-query -Rs '
+alias xi='sudo xbps-install '
+alias l='ls -l'
+alias la='ls -la'
+alias cdv='cd ~/.config/nvim/illu.vim'
+alias edv='cd ~/.config/nvim/illu.vim; nvim'
+alias eds='nvim ~/.shellrc.sh'
+alias cdp='cd ~/Projects'
+
+export VAGRANT_DEFAULT_PROVIDER=libvirt
