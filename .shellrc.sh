eb115e2a3aa63a7ddc2fb599c9b30e39bea63756
Maxim Laurijssen <maxim.laurijssen@gmail.com>
2019-07-03 23:29:28 +0200

    Added diary scripts to terminal.

--- .shellrc.sh
+++ .shellrc.sh
@@ -9,5 +9,9 @@ alias edv='cd ~/.config/nvim/illu.vim; nvim'
 alias eds='nvim ~/.shellrc.sh'
 alias edt='nvim ~/.tmux.conf'
 alias cdp='cd ~/Projects'
+alias cdd="cd ~/Diary"
+alias c='clear'
+alias dentry='cd ~/Diary; DATE=`date +%d-%m-%Y`; FILE="entry_$DATE.md"; touch $FILE; echo "# Diary Entry $DATE" >> $FILE; nvim $FILE'
+alias diary='cd ~/Diary; nvim'
 
 export VAGRANT_DEFAULT_PROVIDER=libvirt
