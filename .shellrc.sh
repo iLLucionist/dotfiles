7a18052f01d1ec9c7d38472289ec5df7518c6a88
Maxim Laurijssen <maxim.laurijssen@gmail.com>
2020-01-05 11:31:36 +0100

    Updated shell script

--- .shellrc.sh
+++ .shellrc.sh
@@ -13,5 +13,8 @@ alias cdd="cd ~/Diary"
 alias c='clear'
 alias dentry='cd ~/Diary; DATE=`date +%d-%m-%Y`; FILE="entry_$DATE.md"; touch $FILE; echo "# Diary Entry $DATE" >> $FILE; nvim $FILE'
 alias diary='cd ~/Diary; nvim'
+alias gs="git status"
 
+export LANG="en_US.UTF-8"
+export PATH=$PATH:~/.yarn/bin/
 export VAGRANT_DEFAULT_PROVIDER=libvirt
